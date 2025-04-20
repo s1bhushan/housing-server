c9bb7bc95a6d913022f4ee0a92a24500a3bf7c09
Bhushan Sonawane <bsonawane990@gmail.com>
2025-04-20 13:46:51 +0530

    dockeradd

--- src/main/java/com/example/housinglogin/security/WebSecurityConfig.java
+++ src/main/java/com/example/housinglogin/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
